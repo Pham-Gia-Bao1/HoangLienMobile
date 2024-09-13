fe14c6e67498d63b1011a2e06f3845a4d6557adb
Bao Pham <bao.pham@sapotacorp.vn>
2024-09-13 08:46:03 +0700

    change path of image customers

--- src/components/sliders/HomeSection4.tsx
+++ src/components/sliders/HomeSection4.tsx
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect } from 'react';
 import Image from 'next/image'; // or your preferred image component
 
-import CustomerImage1 from "../../assets/images/customers/z5823647902998_e600855158906db0e589ed433f8728d3.jpg";
-import CustomerImage2 from "../../assets/images/customers/z5823647908561_b01550f2f7a5cf722a31a013e59b6c47.jpg";
-import CustomerImage3 from "../../assets/images/customers/z5823647932135_195483dbf87c4fcc3c8b09310eb8c422.jpg";
-import CustomerImage4 from "../../assets/images/customers/z5823647962962_e033516079cf55bef905a18ed08e9be1.jpg";
-import CustomerImage5 from "../../assets/images/customers/z5823647965286_81c10f06dce547e401eca6d871c75157.jpg";
-import CustomerImage6 from "../../assets/images/customers/z5823647974354_f720ed82c2bf3c96cea0570aeb98fe5c.jpg";
+import CustomerImage1 from '../../assets/customers/customer1.jpg';
+import CustomerImage2 from '../../assets/customers/customer2.jpg';
+import CustomerImage3 from '../../assets/customers/customer3.jpg';
+import CustomerImage4 from '../../assets/customers/customer4.jpg';
+import CustomerImage5 from '../../assets/customers/customer5.jpg';
+import CustomerImage6 from '../../assets/customers/customer6.jpg';
 
 const Customers = [
   CustomerImage1,
